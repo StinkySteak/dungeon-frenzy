e4dc04dcc5869ca966a4cb231aa45314eaf9b6fe
StinkySteak <slimesteve17@gmail.com>
2024-02-27 10:58:58 +0700

    feat: player jump
    add ground layer
    add ground check

--- Assets/_N2D/Scripts/Gameplay/Player/Character/PlayerCharacterMovement.cs
+++ Assets/_N2D/Scripts/Gameplay/Player/Character/PlayerCharacterMovement.cs
@@ -6,16 +6,39 @@ namespace StinkySteak.N2D.Gameplay.Player.Movement
 {
     public class PlayerCharacterMovement : NetworkBehaviour
     {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
         [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _jumpForce;
+        [SerializeField] private bool _isGrounded;
+        [SerializeField] private Transform _groundChecker;
+        [SerializeField] private LayerMask _groundLayer;
+        [SerializeField] private float _groundCheckOverlapRadius = 0.1f;
 
         public override void NetworkFixedUpdate()
         {
+            _isGrounded = GetIsGrounded();
+
             if (FetchInput(out PlayerCharacterInput input))
             {
                 Vector2 velocity = _moveSpeed * input.HorizontalMove * Sandbox.FixedDeltaTime * Vector2.right;
 
+                if (input.Jump && _isGrounded)
+                {
+                    _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
+                }
+
                 transform.Translate(velocity);
             }
+        }
+
+        private bool GetIsGrounded()
+        {
+            Collider2D ground = Physics2D.OverlapCircle(_groundChecker.position, _groundCheckOverlapRadius, _groundLayer);
+
+            if (ground == null)
+                return false;
+
+            return true;
         }
     }
 }
