13aebf8fed835dc6ce476bd8b5b1e269ecce0512
StinkySteak <slimesteve17@gmail.com>
2024-03-21 19:20:49 +0700

    feat: camera manager
    using cinemachine

---
+++ Assets/_N2D/Scripts/Gameplay/Camera/CameraManager.cs
@@ -0,0 +1,26 @@
+using Cinemachine;
+using Netick.Unity;
+using StinkySteak.N2D.Gameplay.Player.Character;
+using StinkySteak.N2D.Gameplay.PlayerManager.LocalPlayer;
+using StinkySteak.N2D.Netick;
+using UnityEngine;
+
+namespace StinkySteak.N2D.Gameplay.Cam.Manager
+{
+    public class CameraManager : MonoBehaviour, INetickSceneLoaded
+    {
+        [SerializeField] private CinemachineBrain _cinemachineBrain;
+        [SerializeField] private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
+        private void OnCharacterSpawned(PlayerCharacter playerCharacter)
+        {
+            _cinemachineVirtualCamera.Follow = playerCharacter.transform;
+        }
+
+        public void OnSceneLoaded(NetworkSandbox sandbox)
+        {
+            LocalPlayerManager localPlayerManager = sandbox.GetComponent<LocalPlayerManager>();
+            localPlayerManager.OnCharacterSpawned += OnCharacterSpawned;
+        }
+    }
+}
