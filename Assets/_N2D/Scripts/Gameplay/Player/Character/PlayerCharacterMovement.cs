ecb0fb58394e70423039a1de801190824d7682f7
StinkySteak <slimesteve17@gmail.com>
2024-03-22 11:17:50 +0700

    feat: double jump

--- Assets/_N2D/Scripts/Gameplay/Player/Character/PlayerCharacterMovement.cs
+++ Assets/_N2D/Scripts/Gameplay/Player/Character/PlayerCharacterMovement.cs
@@ -2,6 +2,7 @@ using Netick.Unity;
 using Netick;
 using StinkySteak.N2D.Gameplay.PlayerInput;
 using UnityEngine;
+using StinkySteak.Netick.Timer;
 
 namespace StinkySteak.N2D.Gameplay.Player.Character.Movement
 {
@@ -10,7 +11,14 @@ namespace StinkySteak.N2D.Gameplay.Player.Character.Movement
         [SerializeField] private Rigidbody2D _rigidbody2D;
         [SerializeField] private float _moveSpeed;
         [SerializeField] private float _jumpForce;
+        [SerializeField] private float _doubleJumpForce;
+        [SerializeField] private float _doubleJumpDelay = 0.2f;
         [Networked] private bool _isGrounded { get; set; }
+        [Networked] private int _jumpCount { get; set; }
+        [Networked] private bool _jumpButtonPressed { get; set; }
+
+        private TickTimer _timerDoubleJumpDelay;
+
         [SerializeField] private Transform _groundChecker;
         [SerializeField] private LayerMask _groundLayer;
         [SerializeField] private float _groundCheckOverlapRadius = 0.1f;
@@ -19,16 +27,35 @@ namespace StinkySteak.N2D.Gameplay.Player.Character.Movement
         {
             _isGrounded = GetIsGrounded();
 
+            if (_isGrounded)
+            {
+                _jumpCount = 2;
+            }
+
             if (FetchInput(out PlayerCharacterInput input))
             {
-                Vector2 velocity = _moveSpeed * input.HorizontalMove * Sandbox.FixedDeltaTime * Vector2.right;
+                bool jumpButtonWasPressedThisTick = _jumpButtonPressed == false && input.Jump;
 
-                if (input.Jump && _isGrounded)
+                Vector2 velocity = _moveSpeed * input.HorizontalMove * Sandbox.FixedDeltaTime * Vector2.right;
+                
+                // First Jump
+                if (jumpButtonWasPressedThisTick && _jumpCount == 2)
                 {
                     _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
+                    _jumpCount--;
+                    _timerDoubleJumpDelay = TickTimer.CreateFromSeconds(Sandbox, _doubleJumpDelay);
                 }
 
+                // Second Jump
+                if (jumpButtonWasPressedThisTick && _jumpCount == 1 && _timerDoubleJumpDelay.IsExpiredOrNotRunning(Sandbox))
+                {
+                    _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _doubleJumpForce);
+                    _jumpCount--;
+                }
+
+
                 transform.Translate(velocity);
+                _jumpButtonPressed = input.Jump;
             }
         }
 
